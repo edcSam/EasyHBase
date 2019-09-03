d15f1d1103b67247f68708a69322fceae96cd579
杨荣 <yangrong@bbdservice.com>
2019-09-04 00:55:13 +0800

    change some initiation file, add hbase lib.

---
+++ src/main/java/com/samyang/easyhbase/EasyHBaseApplication.java
@@ -0,0 +1,25 @@
+/**
+ * Yang Rong Personal Service
+ * All Rights Reserved @2019
+ */
+package com.samyang.easyhbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * TODO class doc
+ *
+ * @author Yang Rong
+ * @version 1.0
+ * @email samyang3125c@gmail.com
+ * @create 2019-09-04
+ */
+@SpringBootApplication
+public class EasyHBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyHBaseApplication.class, args);
+	}
+
+}
--- src/test/java/com/samyang/easyhbase/EasyHBaseApplicationTests.java
+++ src/test/java/com/samyang/easyhbase/EasyHBaseApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EasyHbaseApplicationTests {
+public class EasyHBaseApplicationTests {
 
 	@Test
 	public void contextLoads() {
