663ab734845f9af1473330ff0d126aa755a50377
杨荣 <yangrong@bbdservice.com>
2019-09-04 00:34:18 +0800

    merge spring boot project

---
+++ src/main/java/com/samyang/easyhbase/EasyHbaseApplication.java
@@ -0,0 +1,13 @@
+package com.samyang.easyhbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyHbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyHbaseApplication.class, args);
+	}
+
+}
